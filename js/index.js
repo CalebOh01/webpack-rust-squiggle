3c21b734d6084bac3d2fdb4cea73b8732ca58522
Caleb Oh <caleboh01@gmail.com>
2020-07-31 10:18:17 -0400

    added basic benchmark

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-import ("../pkg/index.js").then( module => {
-    console.log(module.squiggle('ATGC'))
+import("../pkg/index.js").then(module => {
+    function randomSeq(length) {
+        var result = '';
+        var characters = 'ATGC';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    let seq = randomSeq(1000000);
+    let j = 0
+    while (j < 20) {
+        let then = new Date();
+        module.squiggle(seq);
+        let now = new Date();
+        console.log((now.getTime() - then.getTime()) / 1000);
+        j++;
+    }
 })
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
 use wasm_bindgen::prelude::*;
-// use serde::{Serialize, Deserialize};
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn squiggle(seq: &str) -> JsValue {
-    // define vectors and starting coord
     let mut x: Vec<f32> = vec![0.0];
     let mut y: Vec<f32> = vec![0.0];
     let mut y_coord: f32 = 0.0;
-
-    // loop through entire seq
     for character in seq.chars() {
         x.push(x[x.len() - 1] + 0.5);
         x.push(x[x.len() - 1] + 0.5);
-        // utilizes pattern match syntax for supposed higher performance
         match character {
             'A' => {
                 y.push(y_coord + 0.5);
@@ -37,24 +29,12 @@ pub fn squiggle(seq: &str) -> JsValue {
                 y.push(y_coord + 1.0);
                 y_coord += 1.0;
             }
-            // squiggle by default takes all values
             _ => {
                 y.push(y_coord);
                 y.push(y_coord);
             }
         }
     }
-
     let result: Vec<Vec<f32>> = vec![x, y];
     JsValue::from_serde(&result).unwrap()
-    // convert x and y vectors into arrays
-    // let x_final: Array = x.into_iter().map(JsValue::from).collect();
-    // let y_final: Array = y.into_iter().map(JsValue::from).collect();
-    // declare nested vector
-    // let mut ptr: Vec<Array> = vec![];
-    // ptr.push(x_final);
-    // ptr.push(y_final);
-    // convert vec<array> into nested array
-    // let result: Array = ptr.into_iter().map(JsValue::from).collect();
-    // result 
 }
